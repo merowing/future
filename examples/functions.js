5c8fd159c360095f0b0736f6ea6762ec477ebb18
George <elgynart@gmail.com>
2018-10-23 16:54:46 +0300

    fix funcitons.js

--- examples/functions.js
+++ examples/functions.js
@@ -13,11 +13,15 @@ let func6 = (name, surname) => {
 }
 // якщо немає return, то повернене значення функції буде undefined
 
+// -----
 let arr = [1, 4, 0, 10];
 let len = arr.length;
 let maxValue = 0;
-function getMaxValue(arr, i, maxValue = 0) {
+// maxValue = 0 - значення за замовчуванням, якщо не передається
+//function getMaxValue(arr, i, maxValue = 0) {
+let getMaxValue = (arr, i, maxValue = 0) => {
     if(i <= 0) return maxValue;
     maxValue = (maxValue > arr[i]) ? maxValue : arr[i];
     return getMaxValue(arr, --i, maxValue);
 }
+getMaxValue(arr, (len - 1)); // getMaxValue(any, Array.length - 1);
