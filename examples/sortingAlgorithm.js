0471b00cdfd8384045bca1e3c12a257658db2d06
George <elgynart@gmail.com>
2018-10-24 12:50:25 +0300

    try to realize merge sort

--- examples/sortingAlgorithm.js
+++ examples/sortingAlgorithm.js
@@ -36,3 +36,51 @@
     }
     return arr;
 })([1,3,2,0,-10]);
+
+/**
+ * Insertion sort
+ * 3,1,2,0
+ * 
+ * put 1 before 3
+ * put 2 between 1 and 3
+ * put 0 before 1 2 3
+ */
+(function(arr) {
+    let len = arr.length;
+    let position = 0;
+    let temp;
+    for(let i = 1; i < len; i++) {
+        temp = arr[i];
+        // знаходимо місце в масиві куди поставити наступний елемент
+        // ми робимо порівняння двох одних і тих же самих елементів, щоб оновити position
+        for(let j = i; j >= 0; j--) {
+            if(temp <= arr[j]) {
+                position = j;
+            }
+        }
+        // зміщуємо праворуч всі елементи масиву від знайденої позиції до позиції елементу, який порівнюємо
+        for(let q = i; q > position; q--) {
+            arr[q] = arr[q - 1];
+        }
+        arr[position] = temp;
+    }
+    return arr;
+})([3,1,2,-1,4,3,-2]);
+
+/**
+ * Merge sort
+ */
+(function(arr) {
+    let len = arr.length;
+
+})([1,4,3,2]);
+
+/**
+ * Merge sort. Charter II
+ * 
+ * We have two arrays and we must merge and sort them.
+ * [1,4,3,2] + [6,0,9,7]
+ */
+(function(arr1, arr2){
+
+})([1,4,3,2], [6,0,9,7]);
